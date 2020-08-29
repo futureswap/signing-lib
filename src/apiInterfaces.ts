3991cce4f85bb500f0b040a2cc8c5b3b7dce89a4
mikejonas <motion@gmail.com>
2020-08-28 17:02:17 -0700

    Fix

--- src/apiInterfaces.ts
+++ src/apiInterfaces.ts
@@ -1,6 +1,6 @@
 export interface SupportedToken {
-  publicAddress: string; // gets displayed to the user or use to look up their local wallet balance (unwrapped address)
-  systemAddress: string; // address we use for talking with the system (wrapped address)
-  symbol: string;
-  name: string;
+  publicAddress: string // gets displayed to the user or use to look up their local wallet balance (unwrapped address)
+  systemAddress: string // address we use for talking with the system (wrapped address)
+  symbol: string
+  name: string
 }
